c103d6070b8877cf9e8dd78cf03d7c68879a344e
Fernando <Log-N@outlook.com>
2022-01-03 16:58:12 -0330

    Create automate_helm.py

---
+++ automate_helm.py
@@ -0,0 +1,12 @@
+[4:06 PM] Fernando Santini
+import os
+
+root_path = './'
+
+folders = ['api01_back', 'api02_front', 'api03', 'api04']
+
+gh = foldersprint(gh)
+
+for folder in gh:
+  os.system("/snap/bin/helm create " + folder)
+
